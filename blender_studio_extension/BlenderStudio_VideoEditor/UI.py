d406d9ab6f8bbe1a4d43891bddb5dfc97b9b9989
Franciszek Barszcz <72795920+Franek313@users.noreply.github.com>
2025-04-04 17:11:36 +0300

    Update, new test button and checkbox were added

--- blender_studio_extension/BlenderStudio_VideoEditor/UI.py
+++ blender_studio_extension/BlenderStudio_VideoEditor/UI.py
@@ -2,16 +2,38 @@ import bpy
 
 class VIDEO_PT_my_panel(bpy.types.Panel):
     bl_label = "Video Editor"
-    bl_space_type = 'SEQUENCE_EDITOR'   # albo 'VIEW_3D', jeśli w 3D
+    bl_space_type = 'SEQUENCE_EDITOR'
     bl_region_type = 'UI'
     bl_category = "Blender Studio"
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Hello from the Blender Studio Video Editor!")
+        layout.operator('object.testbutton')
+        layout.label(text="Coco jumbo!")
+        layout.prop(context.scene, 'my_bool_property')
+
+class TestButtonOperator(bpy.types.Operator):
+    bl_idname = "object.testbutton"
+    bl_label = "Slice Strip"
+
+    def execute(self, context):
+        if context.scene.my_bool_property:
+            bpy.ops.sequencer.split(type='SOFT')
+        return {"FINISHED"}
+
+classes_list = [
+    VIDEO_PT_my_panel,
+    TestButtonOperator
+]
 
 def register():
-    bpy.utils.register_class(VIDEO_PT_my_panel)
+    for c in classes_list:
+        bpy.utils.register_class(c)
+
+    bpy.types.Scene.my_bool_property = bpy.props.BoolProperty(name="Pozwól ciachać!",
+        description="Włącza opcję",
+        default=False)
 
 def unregister():
-    bpy.utils.unregister_class(VIDEO_PT_my_panel)
+    for c in classes_list:
+        bpy.utils.unregister_class(c)
